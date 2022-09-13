be86b47d92bb05aa4bbd40ea5e1c29c3b455f564
Smaylle1 <110419022+Smaylle1@users.noreply.github.com>
2022-09-12 23:26:02 -0300

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,25 @@
+// const stage = document.getElementById('stage');
+// const ctx = stage.getContext("2d");
+
+
+//  const trail = [];
+//  let larguraSnake = 5;
+//  let velocidadeY = 0;
+//  let velocidadeX = velocidadeY;
+//  let macaY = 0;
+//  let macax = macaY;
+//  let posicaoX = 10;
+//  let posicaoY = 15;
+
+
+
+
+
+
+
+    
+
+
+
+
+
